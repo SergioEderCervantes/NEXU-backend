34f0ea1bf8ebacfba7f4214535bc0f8697eeaf8e
Sergio Eder Cervantes Rincon <al232682@edu.uaa.mx>
2025-11-18 22:26:49 -0600

    funcion prueba para avance

---
+++ seed/read_data.py
@@ -0,0 +1,18 @@
+import os
+import json
+from app.infraestructure.encription_service import EncryptionManager
+from app.infraestructure.file_service import FileManager
+from app.domain.entities import DbFile
+
+def print_users():
+    # TODO: esto no deberia de estar aqui, pero por ahora funciona
+    enc_manager = EncryptionManager()
+    file_manager = FileManager()
+    
+    token = file_manager.read_file(DbFile.USERS)
+    msg = enc_manager.decrypt_data(token)
+    print(msg)
+
+
+if __name__ == '__main__':
+    print_users()
